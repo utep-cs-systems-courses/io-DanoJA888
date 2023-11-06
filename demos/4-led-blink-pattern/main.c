36c4685aa50a7e34f77138e46d2c9f27ba3f5d4b
DanoJA888 <djaguilar3@miners.utep.edu>
2023-11-05 20:42:18 -0700

    Demo 4, blinking lights from dim to bright every duty cycle, red and green simultaneous

--- demos/4-led-blink-pattern/main.c
+++ demos/4-led-blink-pattern/main.c
@@ -15,28 +15,43 @@ int main(void) {
 }
 
 // global state vars that control blinking
-int blinkLimit = 5;  // duty cycle = 1/blinkLimit
-int blinkCount = 0;  // cycles 0...blinkLimit-1
+int blinkLimitR = 8;  // duty cycle = 1/blinkLimit
+int blinkCountR = 0;  // cycles 0...blinkLimit-1
 int secondCount = 0; // state var representing repeating time 0…1s
+
+int blinkLimitG = 8;
+int blinkCountG = 0;
 
 void
 __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
   // handle blinking 
-  blinkCount ++;
-  if (blinkCount >= blinkLimit) { // on for 1 interrupt period
-    blinkCount = 0;
-    P1OUT |= LED_GREEN;
-  } else		          // off for blinkLimit - 1 interrupt periods
+  blinkCountR ++;
+  if (blinkCountR >= blinkLimitR) { // on for 1 interrupt period
+    blinkCountR = 0;
     P1OUT &= ~LED_GREEN;
+  } else		          // off for blinkLimit - 1 interrupt periods
+    P1OUT |= LED_GREEN;
+
+  blinkCountG ++;
+  if(blinkCountG >= blinkLimitG){
+    blinkCountG = 0;
+    P1OUT &= ~LED_RED;
+  }
+  else
+    P1OUT |= LED_RED;
+  
 
   // measure a second
   secondCount ++;
   if (secondCount >= 250) {  // once each second
     secondCount = 0;
-    blinkLimit ++;	     // reduce duty cycle
-    if (blinkLimit >= 8)     // but don't let duty cycle go below 1/7.
-      blinkLimit = 0;
+    blinkLimitR ++;
+    blinkLimitG ++;           // reduce duty cycle
+    if (blinkLimitR >= 8)     // but don't let duty cycle go below 1/7.
+      blinkLimitR = 0;
+    if (blinkLimitG >= 8)
+      blinkLimitG = 0;
   }
 } 
 
